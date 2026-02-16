129339f125773b7faedc4cbf00d7420160e41bae
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2026-02-16 01:55:15 +0100

    feat: Project setup

---
+++ src/main/java/com/massage/booking/MassageBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.massage.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassageBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MassageBookingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/massage/booking/MassageBookingApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.massage.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class MassageBookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
