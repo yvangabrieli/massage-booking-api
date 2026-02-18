7ccaec35ae27ceb378251857d4ee05cd4dda38e2
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2026-02-18 11:04:09 +0100

    feat : Exceptions classes and GlobalExceptionHandler

---
+++ src/main/java/com/massage/booking/exception/UnauthorizedException.java
@@ -0,0 +1,10 @@
+package com.massage.booking.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class UnauthorizedException extends BusinessException {
+
+    public UnauthorizedException(String message) {
+        super(message, HttpStatus.UNAUTHORIZED);
+    }
+}
