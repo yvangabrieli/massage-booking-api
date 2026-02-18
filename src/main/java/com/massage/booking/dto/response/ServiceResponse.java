e45bd1218bde9387fd66d09c5f5e9a14bc32f2c9
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2026-02-18 12:40:10 +0100

    feat : DTO MassageService

---
+++ src/main/java/com/massage/booking/dto/response/ServiceResponse.java
@@ -0,0 +1,25 @@
+package com.massage.booking.dto.response;
+
+import com.massage.booking.entity.enums.ServiceCategory;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServiceResponse {
+
+    private Long id;
+    private String name;
+    private ServiceCategory category;
+    private Integer durationMinutes;
+    private Integer cleanupMinutes;
+    private String description;
+    private Boolean active;
+    private LocalDateTime createdAt;
+}
