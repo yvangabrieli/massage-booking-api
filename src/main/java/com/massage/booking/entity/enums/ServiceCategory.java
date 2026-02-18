e4d9fa364526f1d7ae3d6f99cf27381ad20e71d5
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2026-02-18 11:08:07 +0100

    feat : entity enums category of massages

---
+++ src/main/java/com/massage/booking/entity/enums/ServiceCategory.java
@@ -0,0 +1,7 @@
+package com.massage.booking.entity.enums;
+
+public enum ServiceCategory {
+    DEEP_TISSUE,    // Toque Profundo 60 and 90 min
+    RELAXING,       // Relax Premium , Siesta Express
+    SPECIALIZED     // Happy Feet, Libera Mi Espalda
+}
